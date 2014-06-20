3127ae630950a5664cd6329e1dd56f0fe1fb4ef4
SirDiazo <diazo@hotmail.com>
2014-06-19 20:41:37 -0600

    Version 1.10 Prerelease 2
    Landing Height code transffred over for TWR1 height display. Does not
    affect top of screen.

---
+++ TWR1/LandingHeight.cs
@@ -0,0 +1,126 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using UnityEngine;
+
+//namespace VerticalVelocity
+//{
+//    [KSPAddon(KSPAddon.Startup.Flight, false)]
+//    public class LHFlight : MonoBehaviour
+//    {
+        
+        //don't actually override display in TWR1
+        //public void LateUpdate() //modify UI in late update or KSP default overrides afaik
+        //{
+
+        //    if (FlightUIController.speedDisplayMode == FlightUIController.SpeedDisplayModes.Surface) //only override if in surface mode
+        //    {
+        //        FlightUIController UI = FlightUIController.fetch;
+        //        UI.alt.setValue(heightToLand());
+        //    }
+           
+        //}
+
+        //public class partDist //part's distace to CelestialBody CoM for distance sort
+        //{
+        //    public Part prt;
+        //    public float dist;
+        //}
+
+        //public double heightToLand() //leave public so other mods can call
+        //{
+        //    double landHeight = 0;
+        //    bool firstRay = true;
+            
+            
+
+        //    if (FlightGlobals.ActiveVessel.LandedOrSplashed) //if landed or splashed, height is 0
+        //    {
+        //        landHeight = 0;
+        //    }
+        //    else if (FlightGlobals.ActiveVessel.altitude - FlightGlobals.ActiveVessel.pqsAltitude > 2400) //raycast goes wierd outside physics range
+        //    {
+        //        landHeight = FlightGlobals.ActiveVessel.altitude - FlightGlobals.ActiveVessel.pqsAltitude; //more then 2400 above ground, just use vessel CoM
+        //    }
+        //    else //inside physics range, goto raycast
+        //    {
+        //        List<Part> partToRay = new List<Part>(); //list of parts to ray
+        //        if (FlightGlobals.ActiveVessel.Parts.Count < 50) //if less then 50 parts, just raycast all parts
+        //        {
+        //            partToRay = FlightGlobals.ActiveVessel.Parts;
+        //        }
+        //        else //if over 50 parts, only raycast the 30 parts closest to ground. difference between 30 and 50 parts to make the sort worth the processing cost, no point in running the sort on 31 parts as the sort costs more processor power then 1 raycast
+        //        {
+        //            List<partDist> partHeights = new List<partDist>(); //only used above 50 parts, links part to distance to ground
+        //            foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+        //            {
+        //                partHeights.Add(new partDist() { prt = p, dist = Vector3.Distance(p.transform.position, FlightGlobals.ActiveVessel.mainBody.position) }); //create list of parts and their distance to ground
+        //                //print("a: " + Vector3.Distance(p.transform.position, FlightGlobals.ActiveVessel.mainBody.position));
+        //            }
+        //            partHeights.Sort((i,j) => i.dist.CompareTo(j.dist)); //sort parts so parts closest to ground are at top of list
+        //            for (int i = 0; i < 30; i = i + 1)
+        //            {
+        //                partToRay.Add(partHeights[i].prt); //make list of 30 parts closest to ground
+        //                //print("b: " + i + " " + partHeights[i].prt.name + " " + partHeights[i].dist + " " + Vector3.Distance(FlightGlobals.ActiveVessel.CoM, FlightGlobals.ActiveVessel.mainBody.position));
+        //            }
+
+        //        }
+
+        //        foreach (Part p in partToRay)
+        //        {
+        //            if (p.collider.enabled) //only check part if it has collider enabled
+        //            {
+        //                Vector3 partEdge = p.collider.ClosestPointOnBounds(FlightGlobals.currentMainBody.position); //find collider edge closest to ground
+        //                RaycastHit pHit;
+        //                Ray pRayDown = new Ray(partEdge, FlightGlobals.currentMainBody.position);
+        //                LayerMask pRayMask = 33792; //layermask does not ignore layer 0, why?
+        //                if (Physics.Raycast(pRayDown, out pHit,(float)(FlightGlobals.ActiveVessel.mainBody.Radius + FlightGlobals.ActiveVessel.altitude),pRayMask)) //cast ray
+        //                {
+
+        //                    if (firstRay) //first ray this update, always set height to this
+        //                    {
+
+        //                                landHeight = pHit.distance;
+                                
+        //                        firstRay = false;
+        //                    }
+        //                    else
+        //                    {
+                                
+        //                                landHeight = Math.Min(landHeight, pHit.distance);
+                                    
+                              
+        //                    }
+        //                    //if (pHit.transform.gameObject.layer != 10 && pHit.transform.gameObject.layer != 15)  //Error trap, ray should only hit layers 10 and 15
+        //                    //{
+        //                    //    print(p.name + " " + pHit.transform.gameObject.layer + " " + pHit.collider.name + " " + pHit.distance);
+        //                    //}
+
+        //                }
+        //                else if (!firstRay) //error trap, ray hit nothing
+        //                {
+        //                    landHeight = FlightGlobals.ActiveVessel.altitude;
+        //                    firstRay = false;
+        //                }
+        //            }
+
+        //        }
+        //        if (landHeight < 1) //if we are in the air, always display an altitude of at least 1
+        //        {
+        //            landHeight = 1;
+        //        }
+        //    }
+
+        //    if (FlightGlobals.ActiveVessel.mainBody.ocean) //if mainbody has ocean we land on water before the seabed
+        //    {
+        //        if (landHeight > FlightGlobals.ActiveVessel.altitude)
+        //        {
+        //            landHeight = FlightGlobals.ActiveVessel.altitude;
+        //        }
+        //    }
+            
+        //    return landHeight;
+        //}
+    }
+}
--- TWR1/TWR1.cs
+++ TWR1/TWR1.cs
@@ -849,29 +849,29 @@ namespace VerticalVelocity
                 TWR1MaxThrust = 1; //set MaxThrust to at least 1 to avoid this
             }
             TWR1VelocityCurrent = TWR1Vessel.verticalSpeed; //set our current vertical velocity
-            if (TWR1Vessel.mainBody.bodyName == "Kerbin" || TWR1Vessel.mainBody.bodyName == "Laythe")
-            {
-                if (TWR1Vessel.heightFromTerrain >= 0)
-                {
-                    TWR1HCToGround = (float)Math.Min(TWR1Vessel.altitude, TWR1Vessel.heightFromTerrain); //use shorter distance, ship to sea level (altitude) or terrain (above ground/building). If you are over water, height to terrain will read to the sea bottom, not sea level, resulting in crashes.
-                }
-                else
-                {
-                    TWR1HCToGround = (float)TWR1Vessel.altitude;
-                }
-            }
-            else
-            {
-                if (TWR1Vessel.heightFromTerrain >= 0)
-                {
-                    TWR1HCToGround = (float)TWR1Vessel.heightFromTerrain;
-                }
-                else
-                {
-                    TWR1HCToGround = (float)TWR1Vessel.altitude;
-                }
-            }
-
+            //if (TWR1Vessel.mainBody.bodyName == "Kerbin" || TWR1Vessel.mainBody.bodyName == "Laythe")
+            //{
+            //    if (TWR1Vessel.heightFromTerrain >= 0)
+            //    {
+            //        TWR1HCToGround = (float)Math.Min(TWR1Vessel.altitude, TWR1Vessel.heightFromTerrain); //use shorter distance, ship to sea level (altitude) or terrain (above ground/building). If you are over water, height to terrain will read to the sea bottom, not sea level, resulting in crashes.
+            //    }
+            //    else
+            //    {
+            //        TWR1HCToGround = (float)TWR1Vessel.altitude;
+            //    }
+            //}
+            //else
+            //{
+            //    if (TWR1Vessel.heightFromTerrain >= 0)
+            //    {
+            //        TWR1HCToGround = (float)TWR1Vessel.heightFromTerrain;
+            //    }
+            //    else
+            //    {
+            //        TWR1HCToGround = (float)TWR1Vessel.altitude;
+            //    }
+            //}
+            TWR1HCToGround = heightToLand();
 
             TWR1HC5Thrust = (Math.Max((TWR1MaxThrust * .05), TWR1MinThrust) / TWR1Mass) - TWR1GravForce; //accel at 5% thrust, makes sure engine is on to allow for ship horizontal speed adjustment. this outside HC method for UI dispaly
             TWR1HC1Thrust = (Math.Max((TWR1MaxThrust * .01), TWR1MinThrust) / TWR1Mass) - TWR1GravForce;
@@ -1159,7 +1159,106 @@ namespace VerticalVelocity
 
 
         }
-        
+        public class partDist //part's distace to CelestialBody CoM for distance sort
+        {
+            public Part prt;
+            public float dist;
+        }
+
+        public double heightToLand() //leave public so other mods can call
+        {
+            double landHeight = 0;
+            bool firstRay = true;
+
+
+
+            if (FlightGlobals.ActiveVessel.LandedOrSplashed) //if landed or splashed, height is 0
+            {
+                landHeight = 0;
+            }
+            else if (FlightGlobals.ActiveVessel.altitude - FlightGlobals.ActiveVessel.pqsAltitude > 2400) //raycast goes wierd outside physics range
+            {
+                landHeight = FlightGlobals.ActiveVessel.altitude - FlightGlobals.ActiveVessel.pqsAltitude; //more then 2400 above ground, just use vessel CoM
+            }
+            else //inside physics range, goto raycast
+            {
+                List<Part> partToRay = new List<Part>(); //list of parts to ray
+                if (FlightGlobals.ActiveVessel.Parts.Count < 50) //if less then 50 parts, just raycast all parts
+                {
+                    partToRay = FlightGlobals.ActiveVessel.Parts;
+                }
+                else //if over 50 parts, only raycast the 30 parts closest to ground. difference between 30 and 50 parts to make the sort worth the processing cost, no point in running the sort on 31 parts as the sort costs more processor power then 1 raycast
+                {
+                    List<partDist> partHeights = new List<partDist>(); //only used above 50 parts, links part to distance to ground
+                    foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+                    {
+                        partHeights.Add(new partDist() { prt = p, dist = Vector3.Distance(p.transform.position, FlightGlobals.ActiveVessel.mainBody.position) }); //create list of parts and their distance to ground
+                        //print("a: " + Vector3.Distance(p.transform.position, FlightGlobals.ActiveVessel.mainBody.position));
+                    }
+                    partHeights.Sort((i, j) => i.dist.CompareTo(j.dist)); //sort parts so parts closest to ground are at top of list
+                    for (int i = 0; i < 30; i = i + 1)
+                    {
+                        partToRay.Add(partHeights[i].prt); //make list of 30 parts closest to ground
+                        //print("b: " + i + " " + partHeights[i].prt.name + " " + partHeights[i].dist + " " + Vector3.Distance(FlightGlobals.ActiveVessel.CoM, FlightGlobals.ActiveVessel.mainBody.position));
+                    }
+
+                }
+
+                foreach (Part p in partToRay)
+                {
+                    if (p.collider.enabled) //only check part if it has collider enabled
+                    {
+                        Vector3 partEdge = p.collider.ClosestPointOnBounds(FlightGlobals.currentMainBody.position); //find collider edge closest to ground
+                        RaycastHit pHit;
+                        Ray pRayDown = new Ray(partEdge, FlightGlobals.currentMainBody.position);
+                        LayerMask pRayMask = 33792; //layermask does not ignore layer 0, why?
+                        if (Physics.Raycast(pRayDown, out pHit, (float)(FlightGlobals.ActiveVessel.mainBody.Radius + FlightGlobals.ActiveVessel.altitude), pRayMask)) //cast ray
+                        {
+
+                            if (firstRay) //first ray this update, always set height to this
+                            {
+
+                                landHeight = pHit.distance;
+
+                                firstRay = false;
+                            }
+                            else
+                            {
+
+                                landHeight = Math.Min(landHeight, pHit.distance);
+
+
+                            }
+                            //if (pHit.transform.gameObject.layer != 10 && pHit.transform.gameObject.layer != 15)  //Error trap, ray should only hit layers 10 and 15
+                            //{
+                            //    print(p.name + " " + pHit.transform.gameObject.layer + " " + pHit.collider.name + " " + pHit.distance);
+                            //}
+
+                        }
+                        else if (!firstRay) //error trap, ray hit nothing
+                        {
+                            landHeight = FlightGlobals.ActiveVessel.altitude;
+                            firstRay = false;
+                        }
+                    }
+
+                }
+                if (landHeight < 1) //if we are in the air, always display an altitude of at least 1
+                {
+                    landHeight = 1;
+                }
+            }
+
+            if (FlightGlobals.ActiveVessel.mainBody.ocean) //if mainbody has ocean we land on water before the seabed
+            {
+                if (landHeight > FlightGlobals.ActiveVessel.altitude)
+                {
+                    landHeight = FlightGlobals.ActiveVessel.altitude;
+                }
+            }
+
+            return landHeight;
+        }   
     }
 
 }
